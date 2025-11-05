2425fcaa273fecc9f0ca0571761d8634e427a332
GAYATHRI BHAVYA <gayathribhavya41@gmail.com>
2025-11-05 16:28:48 +0530

    microsoft login update

--- client/lib/firebaseConfig.ts
+++ client/lib/firebaseConfig.ts
@@ -4,28 +4,22 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// FRONTEND Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyB9JXnOKXVJfiA-hZGTMD98XegCIXziwPY",
   authDomain: "studio-5012646871-facfb.firebaseapp.com",
   projectId: "studio-5012646871-facfb",
   storageBucket: "studio-5012646871-facfb.firebasestorage.app",
   messagingSenderId: "751817350847",
-  appId: "1:751817350847:web:63130d4e749da40259f00a"
+  appId: "1:751817350847:web:63130d4e749da40259f00a",
 };
 
-// init firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase App
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// exports
+// Exports
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// GOOGLE login provider
+// Providers
 export const googleProvider = new GoogleAuthProvider();
-googleProvider.setCustomParameters({
-  client_id: "751817350847-lg3irjlqog288gk0rj6se2tp58vgglil.apps.googleusercontent.com",
-});
-
-// GITHUB login provider
 export const githubProvider = new GithubAuthProvider();
